aaa85057b1e953d293ac94d35c3e40c6ecf44a23
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-12-01 18:00:50 +0000

    RED: expected to find text "Hello world!" in "GET /web"

---
+++ spec/features/sharable_link_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+
+feature 'Shareable link grabs params from the title' do
+  scenario "Displaying 'Hello world!' on the shareable page" do
+  visit '/web?q=cool'
+  expect(page).to have_content('Hello world!')
+  end
+end
