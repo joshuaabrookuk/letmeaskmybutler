1a88cc0e962605357496fe70c72f48f58ea4c5b1
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-12-02 12:25:11 +0000

    RED: expected to find text "Who is Frank Sinatra?" in "GET /search"

--- spec/features/sharable_link_spec.rb
+++ spec/features/sharable_link_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-# require 'sinatra'
-#
-# feature 'Shareable link grabs params from the title' do
-#   scenario "Displaying 'Hello world!' on the shareable page" do
-#   visit '/web?q=cool'
-#   expect(page).to have_content('Hello world!')
-#   end
-# end
+require 'sinatra'
+
+feature 'Shareable link grabs params from the title' do
+  scenario "Displaying 'Hello world!' on the shareable page" do
+  visit 'http://localhost:4567/search?search_query=Who%20is%20Frank%20Sinatra?'
+  expect(page).to have_content('Who is Frank Sinatra?')
+  end
+end
