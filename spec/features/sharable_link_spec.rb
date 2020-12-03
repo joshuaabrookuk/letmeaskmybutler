e523a21efb4841812c53c088b387103bc2ebd8a2
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-12-03 21:57:26 +0000

    RED: Redirection to Ask site Submit is clicked, user redirected

--- spec/features/sharable_link_spec.rb
+++ spec/features/sharable_link_spec.rb
@@ -19,3 +19,11 @@ feature 'Shareable link grabs params from the title' do
     expect(page).to have_field('search', with: 'Who is Frank Sinatra?')
   end
 end
+
+feature 'Redirection to Ask site' do
+  scenario "Submit is clicked, user redirected" do
+    visit 'http://localhost:4567/search?search_query=Who%20is%20Frank%20Sinatra?'
+    click_button 'Submit'
+    expect(page).to have_current_path('/redirect')
+  end
+end
