e78b6d2788388a8f47ec325643dce913ba06e53b
joshuaabrookuk <joshuabrookuk@gmail.com>
2021-05-30 19:53:04 +0100

    Fixed redirect loop issue

--- public/script_src.js
+++ public/script_src.js
@@ -1,37 +1,40 @@
-var i = 0
-
-var txt = $(<%=@search_query%>)
-
-
-var speed = 125
-
-function typeWriter () {
-
-
-  document.getElementById('search').value += txt.charAt(i)
-  i++
-  setTimeout(typeWriter, speed)
-
-  if (document.getElementById('search').value === txt) {
-    runAnim()
-  }
-}
-
-function pauseAnim () {
-  document.getElementById('animation').style.animationPlayState = 'paused'
-}
-
-function runAnim () {
-  document.getElementById('animation').style.animationPlayState = 'running'
-
-  setTimeout(function () { submitForm() }, 3100)
-}
-
-function submitForm () {
-  document.getElementById('form').submit()
-}
-
-function animSequence () {
-  setTimeout(function () { pauseAnim() }, 2500)
-  setTimeout(function () { typeWriter() }, 1500)
-}
+// var i = 0
+//
+// function typeWriter () {
+//   var txt = '<%=@search_query%>'
+//   var speed = 125
+//
+//   document.getElementById('search').value += txt.charAt(i)
+//   i++
+//   setTimeout(typeWriter, speed)
+//
+//   if (document.getElementById('search').value === txt) {
+//     runAnim()
+//   }
+// }
+//
+// function pauseAnim () {
+//   document.getElementById('animation').style.animationPlayState = 'paused'
+// }
+//
+//
+// var runAnim = (function () {
+//   var executed = false
+//   return function () {
+//     if (!executed) {
+//       executed = true
+//       document.getElementById('animation').style.animationPlayState = 'running'
+//       setTimeout(function () { pauseAnim() }, 3000)
+//       setTimeout(function () { submitForm() }, 3000)
+//     }
+//   }
+// })()
+//
+// function submitForm () {
+//   document.getElementById('form').submit()
+// }
+//
+// function animSequence () {
+//   setTimeout(function () { pauseAnim() }, 2500)
+//   setTimeout(function () { typeWriter() }, 1500)
+// }
